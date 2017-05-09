b9bb2becb06571051d61a4de44ec03eba1fe25e0
sveltedonut <harryjiang0114@gmail.com>
2017-05-09 19:33:48 -0400

    CSS ordering change

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import * as firebase from 'firebase';
-import 'bootstrap/dist/css/bootstrap.css';
 
 class Page extends React.Component {
     constructor() {
